e9c91f6f5da150da4304cd772f790891947be656
Zloyday56 <fas_95@list.ru>
2024-07-30 14:31:22 +0500

    Add files via upload

---
+++ Value.java
@@ -0,0 +1,28 @@
+package com.github.dhmel;
+
+public class Value {
+
+    private int id;
+    private String value;
+
+    @Override
+    public String toString(){
+        return "id: " + id + ", value: " + value;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+}
